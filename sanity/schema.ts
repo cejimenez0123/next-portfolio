1cb6b2ec32586b03c92c38bc4cb5dd7b7ddfb9d8
cejimenez0123 <christianjimenez0123@gmail.com>
2023-06-23 16:12:05 -0400

    init

--- sanity/schema.ts
+++ sanity/schema.ts
@@ -1,5 +1,27 @@
 import { SchemaTypeDefinition } from 'sanity'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [],
+  types: [{
+  title: 'Article',
+  name: 'article',
+  type: 'document',
+  fields: [
+    {
+      title: 'Title',
+      name: 'title',
+      type: 'string'
+    },
+    {
+      title: 'Image',
+      name: 'image',
+      type: 'image'
+    },
+    {
+      title: 'Content',
+      name: 'content',
+      type: 'string',
+      
+    }
+  ]
+}],
 }
